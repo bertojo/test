cd8b4bae7b1642789ee6057a429da8dbf0983d3a
Albert Yeoh <bertthings@gmail.com>
2016-10-20 21:04:31 +0800

    test

---
+++ All men are equal/Assets/Scripts/SmallGuyController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallGuyController : MonoBehaviour
+{
+    public float speed;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    private bool touchingDoor = false;
+    public bool onGround = false;
+    Vector2 lookDirection = Vector2.zero;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookDirection = (mousePos - (Vector2)transform.position).normalized;
+        transform.right = lookDirection;
+    }
+    void FixedUpdate()
+    {
+        Rigidbody2D rgbody = GetComponent<Rigidbody2D>();
+        Vector3 velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (onGround) velocity += Vector3.up * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += Vector3.right * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (touchingDoor)
+            {
+
+            }
+        }
+        rgbody.velocity = velocity;
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.rotation = this.transform.rotation;
+            newBullet.GetComponent<Rigidbody2D>().velocity = (bulletSpeed + speed) * lookDirection;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+		if (other.gameObject.CompareTag ("floor"))
+			onGround = true;
+		if (other.gameObject.CompareTag ("door"))
+			touchingDoor = true;
+	}
+    void OnTriggerExit2D(Collider2D other)
+    {
+		if (other.gameObject.CompareTag ("floor"))
+			onGround = false;
+    }
+}
---
+++ All men are equal/Assets/Scripts/playerController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float speed = 10;
+	public float jump = 200;
+	public float throwSpeed = 50;
+    public CircleCollider2D player1Coll;
+
+    private Rigidbody2D rb2d;
+    private bool isRight = false;
+
+	float moveVelocity;
+	private Rigidbody2D player2rb2d;
+	bool onGround = true;
+   
+	bool throwReady;
+
+	// Use this for initialization
+	void Start () {
+		player2rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        rb2d = this.GetComponent<Rigidbody2D>();
+	}
+
+
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 velo = rb2d.velocity;
+        moveVelocity = 0;
+        //left-right movements
+        if (Input.GetKey(KeyCode.W))
+        { if (onGround) {
+                velo.y = jump * Time.deltaTime;
+                rb2d.velocity = velo;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velo.x = speed*Time.deltaTime;
+            rb2d.velocity = velo;
+
+        }
+
+
+        //jump
+        if (Input.GetKey(KeyCode.A))
+        {
+            velo.x = -speed * Time.deltaTime;
+            rb2d.velocity = velo;
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftShift)){
+
+        	if(throwReady){
+                velo.y = throwSpeed * Time.deltaTime;
+                player2rb2d.velocity = velo;
+                print(throwReady);
+
+ 
+            }
+        }
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//print(onGround);
+		if (other.gameObject.CompareTag("floor")){
+			//print(onGround);
+			onGround = true;
+		}
+		if (other.gameObject.CompareTag("Player")){
+			throwReady = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.CompareTag("floor")){
+			//print(onGround);
+			onGround = false;
+		}
+		if (other.gameObject.CompareTag("Player")){
+			throwReady = false;
+		}
+
+	}
+}
